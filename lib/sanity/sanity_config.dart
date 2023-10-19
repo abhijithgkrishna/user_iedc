aa6696e7fedd27b5a125521044cd4a525896e7e1
Abhijith G <95066129+abhijithgkrishna@users.noreply.github.com>
2023-10-19 18:59:43 +0530

    removed key

--- lib/sanity/sanity_config.dart
+++ lib/sanity/sanity_config.dart
@@ -5,7 +5,7 @@ final sanityClient = SanityClient(
   dataset: 'production',
   projectId: 'i0p2y232',
   token:
-      'skqvnPfRtWT2HoFdHEwayz0MWwIH9ewa2Te9Abevs0fE0eAS4Awts5OP6NHUgiYxbbMe8IIaJp585R7jaaFPFCzk4BjyYByh19v3JBe7jmX0h4GRudIzO1iRsPjcYgeMcIVRefJPfIBGxDWc176SS0JUKDEkPzGZQEqcP85uKW4iRWXPj98B',
+      '',
 );
 
 final builder = ImageUrlBuilder(sanityClient);
